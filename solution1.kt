952b91aae196a7cd579edbb4f6b4179bc6ca952c
Faisal Aleissa <mr.faisal.aleissa@gmail.com>
2023-01-01 18:55:51 +0300

    Add files via upload

---
+++ solution1.kt
@@ -0,0 +1,13 @@
+package com.example.tuwaiqfirstprojectforandroid
+
+fun main() {
+    var name : String = "Abdullah"
+    var t : Float = 20.6F
+    var d : Double= 8.9746325
+    var n: Byte = 25
+    var year : Int = 2025
+    var long : Long = 321984526954
+    var b : Boolean = true
+    var c : Char = 'c'
+
+}
